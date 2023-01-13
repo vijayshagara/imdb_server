88a1453acf36c4adb82fc20a0a6762facfba81aa
vijay <vijayshagara1221@gmail.com>
2023-01-13 21:33:31 +0530

    router setting

---
+++ app/config.js
@@ -0,0 +1,9 @@
+const config = {
+  appName: process.env.APP_NAME || "api",
+  appPort: process.env.APP_PORT || 9000,
+};
+
+
+module.exports={
+    config
+}
---
+++ app/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const env = require("dotenv")
+const app = express();
+env.config()
+
+app.post("/",(req,res,next)=>{
+    res.status(200).send({
+        "message":"Post method is calling in /"
+    })
+})
+app.get("/",(req,res,next)=>{
+    res.status(200).send({
+        "message":"Get method is calling in /"
+    })
+})
+
+app.use((req,res,next)=>{
+    return res.status(404).send({
+        "message":"Router Not Found"
+    })
+})
+app.listen(process.env.APP_PORT ,(err)=>{
+    if(err){
+        console.error(`cannot run on port 4567 ${process.env.APP_PORT}`);
+        process.exit(1)
+    }else{
+        console.log(`app running successfully in port ${process.env.APP_PORT}`);
+    }
+})
